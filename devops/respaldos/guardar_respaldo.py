3718cc7d6de283225c69cf2eff44c3c574e9daa7
Aleph-GORY <aleph_g@ciencias.unam.mx>
2024-07-15 22:04:40 -0600

    [feat] devops: Simplificación de conexión al servidor pulsar

--- devops/respaldos/guardar_respaldo.py
+++ devops/respaldos/guardar_respaldo.py
@@ -1,83 +1,46 @@
+import pendulum
+from airflow.decorators import dag, task
+from airflow.providers.ssh.operators.ssh import SSHOperator
+from airflow.providers.ssh.hooks.ssh import SSHHook
 
-from datetime import datetime, timedelta
-from textwrap import dedent
 
-# The DAG object; we'll need this to instantiate a DAG
-from airflow import DAG
+cmd = (
+    'docker run --rm '
+    '-v "$VOLUME_NAME":/backup-volume '
+    '-v "$(pwd)":/backup '
+    'ubuntu '
+    'tar -zcvf /backup/$VOLUME_NAME.tar.gz /backup-volume '
+)
 
-# Operators; we need this to operate!
-from airflow.operators.bash import BashOperator
-with DAG(
-    'tutorial',
-    # These args will get passed on to each operator
-    # You can override them on a per-task basis during operator initialization
-    default_args={
-        'depends_on_past': False,
-        'email': ['airflow@example.com'],
-        'email_on_failure': False,
-        'email_on_retry': False,
-        'retries': 1,
-        'retry_delay': timedelta(minutes=5),
-        # 'queue': 'bash_queue',
-        # 'pool': 'backfill',
-        # 'priority_weight': 10,
-        # 'end_date': datetime(2016, 1, 1),
-        # 'wait_for_downstream': False,
-        # 'sla': timedelta(hours=2),
-        # 'execution_timeout': timedelta(seconds=300),
-        # 'on_failure_callback': some_function,
-        # 'on_success_callback': some_other_function,
-        # 'on_retry_callback': another_function,
-        # 'sla_miss_callback': yet_another_function,
-        # 'trigger_rule': 'all_success'
-    },
-    description='A simple tutorial DAG',
-    schedule_interval=timedelta(days=1),
-    start_date=datetime(2021, 1, 1),
+@dag(
+    default_args={'owner': 'airflow'},
+    schedule=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz='UTC'),
     catchup=False,
-    tags=['example'],
-) as dag:
+    tags=['pulsar','respaldos'],
+    params={
+        'cmd': 'ls'
+    }
+)
+def pulsar_guardar_respaldo():
+    @task()
+    def create_or_update_conn(**context):
+        data = context["dag_run"].conf
 
-    # t1, t2 and t3 are examples of tasks created by instantiating operators
-    t1 = BashOperator(
-        task_id='print_date',
-        bash_command='date',
-    )
+    @task()
+    def run_cmd(**context):
+        data = context["dag_run"].conf
 
-    t2 = BashOperator(
-        task_id='sleep',
-        depends_on_past=False,
-        bash_command='sleep 5',
-        retries=3,
-    )
-    t1.doc_md = dedent(
-        """\
-    #### Task Documentation
-    You can document your task using the attributes `doc_md` (markdown),
-    `doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
-    rendered in the UI's Task Instance Details page.
-    ![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
+        ssh_hook = SSHHook(ssh_conn_id='pulsar')
+        run_cmd = SSHOperator(
+            task_id='run_cmd',
+            ssh_hook=ssh_hook,
+            command=(data['cmd'])
+        )
 
-    """
-    )
+        run_cmd.execute(context=context)
 
-    # dag.doc_md = __doc__  # providing that you have a docstring at the beginning of the DAG
-    dag.doc_md = """
-    This is a documentation placed anywhere
-    """  # otherwise, type it like this
-    templated_command = dedent(
-        """
-    {% for i in range(5) %}
-        echo "{{ ds }}"
-        echo "{{ macros.ds_add(ds, 7)}}"
-    {% endfor %}
-    """
-    )
+    create_or_update_conn() >> run_cmd()
 
-    t3 = BashOperator(
-        task_id='templated',
-        depends_on_past=False,
-        bash_command=templated_command,
-    )
 
-    t1 >> [t2, t3]
+pulsar_guardar_respaldo = pulsar_guardar_respaldo()
